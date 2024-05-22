c5a0db344acddc64d43eb54c934b7d53ef776364
amirulkanak <coder.kanak@gmail.com>
2024-05-22 23:46:54 +0600

    Loading transition

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,9 @@ import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 // Header component
 import Header from '@/components/Header';
+// PageTransition component
+import PageTransition from '@/components/PageTransition';
+import StairTransition from '@/components/StairTransition';
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
@@ -20,7 +23,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
